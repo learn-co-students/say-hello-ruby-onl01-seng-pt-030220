17c4c0ad913777dae3434cee4ac4b6019bb3f373
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-03 03:58:35 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,13 +1,11 @@
 # Build your say_hello method here
 
 def say_hello(name = "Ruby Programmer")
-  puts ("Hello #{name}")
+
+  puts ("Hello #{name}!")
 
 end
 
-def say_hello(name1)
-  puts ("Hello Kent Beck")
 
-end
 
 say_hello
